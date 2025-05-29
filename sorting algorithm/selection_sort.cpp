8a308201742245a07e33dc7e5fedfd0b0e928076
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-05-29 22:15:42 +0600

    sorting algo's

---
+++ sorting algorithm/selection_sort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>a;
+    a={12,2,34,100,43,6,87};
+    int min_index=0;
+
+    for(int i=0;i<a.size();i++){
+        min_index=i;
+        for(int j=i+1;j<a.size();j++){
+            if (a[min_index]>a[j]){
+                min_index=j;
+            }
+        }
+        swap(a[i],a[min_index]);
+    }
+
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+}
