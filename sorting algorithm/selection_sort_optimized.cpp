b42d75453b7eef4c3515684c6b66635612515289
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-06-01 12:29:22 +0600

    selection sort

--- sorting algorithm/selection_sort_optimized.cpp
+++ sorting algorithm/selection_sort_optimized.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int>a;
-    a={12,2,34,100,43,6,87};
-    int min_index=0;
+    vector<int>a={1,0,7,4,5,7,3,5,67,9};
 
-    for(int i=0;i<a.size();i++){
-        min_index=i;
+    for(int i=0;i<a.size()-1;i++){
+        int min_index=i;
         for(int j=i+1;j<a.size();j++){
-            if (a[min_index]>a[j]){
+            if(a[min_index]>a[j]){
                 min_index=j;
             }
         }
+
         swap(a[i],a[min_index]);
     }
 
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
+
 }
