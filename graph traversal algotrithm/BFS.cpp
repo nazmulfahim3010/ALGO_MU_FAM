468edf850f67f6895fe7f4ee64c1f9167d38111e
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-06-30 00:45:14 +0600

    BFS

--- graph traversal algotrithm/BFS.cpp
+++ graph traversal algotrithm/BFS.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 const int MAX=100;
 int vertex,edge;
-int path[MAX]; 
+
 vector<int>graph[MAX];
 vector<int> path;
-void BFS(src);
+void BFS(int src);
 
 int main(void){
     cout<<"insert vertex: ";
@@ -16,21 +16,32 @@ int main(void){
     for(int i=0;i<edge;i++){
         int a,b;
         cin>>a>>b;
-        graph[a].push_bac(b);
+        graph[a].push_back(b);
         graph[b].push_back(a);
     }
     int src;
+    cout<<"root node: "; 
+    cin>>src;
     BFS(src);
     
 
 }
-void BFS(src){
+void BFS(int  src){
     bool visit[MAX]={false};
     queue<int>temp;
-    int item;
+    int node_value;
     visit[src]=true;
     temp.push(src);
     while(!temp.empty()){
-        
+         node_value=temp.front();
+         cout<<node_value<<" ";
+         temp.pop();
+
+        for(auto v : graph[node_value]){
+            if(visit[v]==false){
+                visit[v]=true;
+                temp.push(v);
+            }
+        }
     }
 }
