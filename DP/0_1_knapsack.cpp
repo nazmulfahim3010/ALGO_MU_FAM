45762d01f38f9dd03ca6900328fe85ec647ed3b4
nazmulfahim000 <nazmulfahim000@gmail.com>
2025-08-11 11:31:11 +0600

    01_with out dp

--- DP/0_1_knapsack.cpp
+++ DP/0_1_knapsack.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int knapsack(vector<int>&p,vector<int>&v,int w);
+int knapsack(vector<int>&p,vector<int>&v,int w,int item);
 int main(){
-    vector<int>p;
-    vector<int>v;
+    
     int n;
     cin>>n;
+    vector<int>p(n);
+    vector<int>v(n);
     int w;
     cin>>w;
 
@@ -20,20 +21,27 @@ int main(){
     
 
 
-    cout<<knapsack(p,v,w)<<endl;
+    cout<<knapsack(p,v,w,n)<<endl;
 
 }
 
-int knapsack(vector<int> &p,vector<int> &v,int w){
-    int item=v.size();
+int knapsack(vector<int> &p,vector<int> &v,int w,int item){
+
     vector<vector<int>> dp(item+1,vector<int>(w+1,0));
 
-    for(int i=0;i<item;i++){
-        for(int j=0;j<w;j++){
-            dp[i][j]=max(dp[i-1][w],dp[i-1][j-(w-v[i])]+p[i]);
+    for(int i=1;i<=item;i++){
+        for(int j=0;j<=w;j++){
+            if(v[i-1]<=j){
+                dp[i][j]=max(dp[i-1][j],dp[i-1][j-v[i-1]]+p[i-1]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
         }
     }
     return dp[item][w];
 
-
+ 
 }
+
+///source:https://www.geeksforgeeks.org/dsa/0-1-knapsack-problem-dp-10/
